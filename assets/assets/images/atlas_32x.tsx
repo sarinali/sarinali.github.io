3565bc98c6cc8ca78bc20148ea1d46fa05c4daee
Sarina Li <sarinajin.li@gmail.com>
2022-04-28 01:14:44 -0400

    new build for jaqueline

---
+++ assets/assets/images/atlas_32x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="atlas_32x" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="Super_Retro_World_Interior_pack_week16/Package/atlas_32x.png" width="1536" height="512"/>
+</tileset>
---
+++ assets/assets/images/table.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="table" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="New Piskel (4).png" width="96" height="128"/>
+</tileset>
